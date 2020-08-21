e324c414bbb75db6b80a827c1d6f92c55371268c
Gyanwati Kumari <gyan1990ex@gmail.com>
2020-08-21 14:35:30 +0530

    cc

--- week08day04.py
+++ week08day04.py
@@ -1,22 +1,15 @@
 #Q.https://leetcode.com/problems/kth-largest-element-in-an-array/
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return self.quickSelect(nums,0,len(nums)-1,k)
-    
-    def quickSelect(self,nums,start,n,k):
-        pos = self.kthElement(nums,start,n)
-        if pos == k-1:
-            return nums[pos]
-        elif pos >= k:
-            return self.quickSelect(nums,start,pos-1,k)
-        return self.quickSelect(nums,start,pos+1,k)
-    
-    def kthElement(self,nums,left,right):
-        x = nums[right]
-        i = left
-        for j in range (left,right):
-            if nums[j]>x:
-                nums[j],nums[i]=nums[i],nums[j]
-                i += 1
-            nums[right],nums[i]=nums[i],nums[right]
-        return i
+
+        top_k = nums[0:k]
+        top_k.sort(reverse = True)
+
+        for i in nums[k:]:
+            if i > top_k[-1]:
+                top_k.pop() 
+                top_k.append(i) 
+                top_k.sort(reverse = True) 
+
+        return top_k[-1]
+   
