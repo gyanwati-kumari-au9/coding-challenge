dd3d0d076651d6fa8287aba2ffc06892d4db508f
Gyanwati Kumari <gyan1990ex@gmail.com>
2020-08-24 14:56:51 +0530

    cc

---
+++ week09day01.py
@@ -0,0 +1,23 @@
+#Question 1: Solve the wine dp problem.
+dp=[[]]
+def maxProfit(P,start, end,n, year):
+
+    if (start > end):
+        return 0
+    elif (dp[start][end] != None):
+        return dp[start][end]
+    else:
+        dp[start][end] = max(P[start] * year + maxProfit(P, start + 1, end, n, year + 1), P[end] * year + maxProfit(P, start, end - 1, n, year + 1))
+        return dp[start][end]
+    
+
+if __name__=="__main__":
+    w=int(input())
+    p=input()
+    p=p.split()
+    for idx in range(len(p)):
+        p[idx]=int(p[idx])
+    dp=[[None for i in range(100)] for i in range (100)]
+            
+    res=maxProfit(p,0,w-1,w,1)
+    print(res)
