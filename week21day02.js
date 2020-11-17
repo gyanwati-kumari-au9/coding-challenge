45b3bc65f79ce736016f9dc514a9c115b26838c2
Gyanwati Kumari <gyan1990ex@gmail.com>
2020-11-17 22:36:37 +0530

    cc

--- week21day02.js
+++ week21day02.js
@@ -40,3 +40,30 @@
 // • 0 <= s.length <= 200
 // • s consists of English letters (lower-case and upper-case), digits, ' ', '+', '-
 // ' and '.'.
+
+let atoi = function(str) {
+    let filter = '0123456789+- '
+    let res = 0
+    let sign = 1
+    for(let char of str){
+        let index = filter.indexOf(char)
+        if(index != -1) {
+            if(char == " ") continue
+            if(filter[10] == "+") filter = filter.slice(0,10)
+            if(char == "+") continue
+            if(char == "-") { sign = -sign; continue }
+            res = res*10 + index
+        } else {
+            break;
+        }
+    }
+    res = res*sign
+    if(res > 2**31-1) res = 2**31-1
+    else if (res < -(2**31)) res = -(2**31)
+    return res
+};
+
+
+// string = "words and 987";
+string = "42";
+console.log(atoi(string));
